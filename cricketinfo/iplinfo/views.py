0f07814d00a2bbc9bfaec3e4c3f8146994dc3fde
Jaydip ahir <141310901+jkahir7@users.noreply.github.com>
2023-12-22 11:52:58 +0530

    Initial commit

---
+++ cricketinfo/iplinfo/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request,"iplinfo/index.html")
+
+
+def about(request):
+    return render(request,"iplinfo/about.html")
+
+def about1(request):
+    return render(request,"iplinfo/about1.html")
+
+
+def about2(request):
+    return render(request,"iplinfo/about2.html")
+
+def about3(request):
+    return render(request,"iplinfo/about3.html")
+
+def about4(request):
+    return render(request,"iplinfo/about4.html")
+
+def about5(request):
+    return render(request,"iplinfo/about5.html")
+
+def about6(request):
+    return render(request,"iplinfo/about6.html")
+
+def about7(request):
+    return render(request,"iplinfo/about7.html")
+
+def about8(request):
+    return render(request,"iplinfo/about8.html")
+
+def about9(request):
+    return render(request,"iplinfo/about9.html")
